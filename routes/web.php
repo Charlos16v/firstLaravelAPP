54eb572b5ca42d32184a1507c328a2accacf556e
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2021-11-29 12:43:20 +0100

    feat: post

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Models\Car;
-
+use Illuminate\Support\Facades\Request;
 
 Route::get('/', function () {
     // $cars = DB::select('select * from car');
@@ -13,7 +13,11 @@ Route::get('/', function () {
 });
 
 // Parametro id no obligatorio por defecto valor 1.
-Route::get('/car/{id?}', function ($id = 1) {
+Route::get('/cars/{id?}', function ($id = 1) {
     $result = Car::find($id);
     return $result;
+});
+
+Route::post('/cars', function (Request $request) {
+    return $request->request->all();
 });
