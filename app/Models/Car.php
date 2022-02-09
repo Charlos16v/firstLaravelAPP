e5f239fbd078cb5cb88d4bfc2293dc7211e77fd1
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2022-02-09 16:47:12 +0100

    feat: created Car model and migration of cars table

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -5,8 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model{
-    protected $table = 'car';
 
-    public $color;
-    public $type;
+    /* No seria necesario automaticamente hara el plural para el nombre de la tabla.
+    protected $table = 'cars'; */
+
+    public $brand;
+    public $model;
+    public $cv;
+
+    protected $fillable = [
+        'user_id',
+        'brand',
+        'model',
+        'cv',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ database/migrations/2022_02_03_150455_create_table_cars.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCars extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('brand');
+            $table->string('model');
+            $table->integer('cv');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id')
+                ->nullable()
+                ->after('id');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
